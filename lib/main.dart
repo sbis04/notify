365605dc5a8795356666e9ae1ab48d6bc3b0335d
Souvik Biswas <sbis1999@gmail.com>
2021-02-10 17:07:45 +0530

    Update main to receive push notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,92 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:notify/model/push_notification.dart';
+import 'package:overlay_support/overlay_support.dart';
+// import 'package:notify/utils/notification_client.dart';
 
-void main() {
+GlobalKey notificationKey;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  // NotificationClient.registerNotification();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Notify',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+    return OverlaySupport(
+      child: MaterialApp(
+        title: 'Notify',
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: MyHomePage(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static FirebaseMessaging messaging = FirebaseMessaging();
+
+  PushNotification _notificationInfo;
+  int _totalNotifications;
+
+  void registerNotification() async {
+    // On iOS, this helps to take the user permissions
+    await messaging.requestNotificationPermissions(
+      IosNotificationSettings(
+        alert: true,
+        badge: true,
+        provisional: false,
+        sound: true,
+      ),
+    );
+
+    messaging.configure(
+      onMessage: (message) async {
+        print('onMessage received: $message');
+
+        PushNotification notification = PushNotification.fromJson(message);
+
+        setState(() {
+          _notificationInfo = notification;
+          _totalNotifications++;
+        });
+
+        showSimpleNotification(
+          Text(_notificationInfo.title),
+          leading: NotificationBadge(totalNotifications: _totalNotifications),
+          subtitle: Text(_notificationInfo.body),
+          background: Colors.cyan[700],
+          duration: Duration(seconds: 2),
+        );
+      },
+    );
+
+    messaging.getToken().then((token) {
+      print('Token: $token');
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  @override
+  void initState() {
+    _totalNotifications = 0;
+    registerNotification();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,8 +105,82 @@ class HomePage extends StatelessWidget {
               fontSize: 20,
             ),
           ),
+          SizedBox(height: 16.0),
+          NotificationBadge(totalNotifications: _totalNotifications),
+          SizedBox(height: 16.0),
+          _notificationInfo != null
+              ? Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'TITLE: ${_notificationInfo.title}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      'BODY: ${_notificationInfo.body}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ],
+                )
+              : Container(),
         ],
       ),
     );
   }
 }
+
+class NotificationBadge extends StatelessWidget {
+  final int totalNotifications;
+
+  const NotificationBadge({@required this.totalNotifications});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 40.0,
+      height: 40.0,
+      decoration: new BoxDecoration(
+        color: Colors.red,
+        shape: BoxShape.circle,
+      ),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            '$totalNotifications',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// class MessageNotification extends StatelessWidget {
+//   final PushNotification notification;
+
+//   MessageNotification({@required this.notification});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Card(
+//       margin: const EdgeInsets.symmetric(horizontal: 4),
+//       child: SafeArea(
+//         child: ListTile(
+//           // leading: SizedBox.fromSize(
+//           //     size: const Size(40, 40),
+//           //     child: ClipOval(child: Image.asset('assets/avatar.png'))),
+//           title: Text('Boyan'),
+//           subtitle: Text(message),
+//         ),
+//       ),
+//     );
+//   }
+// }
