646ac7d459dcd6054b7de34da60ffd674f864f81
Souvik Biswas <sbis1999@gmail.com>
2021-02-12 13:15:58 +0530

    Update the model class

--- lib/model/push_notification.dart
+++ lib/model/push_notification.dart
@@ -2,15 +2,21 @@ class PushNotification {
   PushNotification({
     this.title,
     this.body,
+    this.dataTitle,
+    this.dataBody,
   });
 
   String title;
   String body;
+  String dataTitle;
+  String dataBody;
 
   factory PushNotification.fromJson(Map<String, dynamic> json) {
     return PushNotification(
       title: json["notification"]["title"],
       body: json["notification"]["body"],
+      dataTitle: json["data"]["title"],
+      dataBody: json["data"]["body"],
     );
   }
 }
